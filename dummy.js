952df6fac01085d8f85fab188c8aa9f9fd54b257
Krishnat Mahajan <mahajankrishna798@gmail.com>
2024-04-10 17:09:18 +0530

    commited changes in dummy.js

---
+++ dummy.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+  
+  const str = 'Hello, World!';
+  const reversedStr = reverseString(str);
+  console.log(reversedStr); // Output:!dlroW,olleH
