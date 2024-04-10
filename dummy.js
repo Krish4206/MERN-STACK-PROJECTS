2b56df8d77fceb9ca5eb778981ed7947456377ec
Krishnat Mahajan <mahajankrishna798@gmail.com>
2024-04-10 17:32:28 +0530

    modified dummy.js

--- dummy.js
+++ dummy.js
@@ -2,6 +2,7 @@ function reverseString(str) {
     return str.split('').reverse().join('');
   }
   
-  const str = 'Hello, World!';
+  const str = 'Hello, Version control system!';
+
   const reversedStr = reverseString(str);
   console.log(reversedStr); // Output:!dlroW,olleH
